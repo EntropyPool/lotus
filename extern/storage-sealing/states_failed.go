771d88d0ce0824f647134e5cc1aecdf2928a45db
liqh <codex8080@163.com>
2020-09-06 12:10:50 +0000

    Do not remove good precommit1 sectors

--- extern/storage-sealing/states_failed.go
+++ extern/storage-sealing/states_failed.go
@@ -55,8 +55,8 @@ func (m *Sealing) handleSealPrecommit1Failed(ctx statemachine.Context, sector Se
 	}
 
 	log.Warnf("pre commit1 fails(%v) sector (%v)", sector.PreCommit1Fails, sector.SectorNumber)
-	if sector.PreCommit1Fails > 2 {
-		return ctx.Send(SectorRemove{})
+	if sector.PreCommit1Fails > 5 {
+		// return ctx.Send(SectorRemove{})
 	}
 
 	return ctx.Send(SectorRetrySealPreCommit1{})
@@ -69,7 +69,7 @@ func (m *Sealing) handleSealPrecommit2Failed(ctx statemachine.Context, sector Se
 
 	log.Warnf("pre commit2 fails(%v) sector (%v)", sector.PreCommit2Fails, sector.SectorNumber)
 	if sector.PreCommit2Fails > 5 {
-		return ctx.Send(SectorRetrySealPreCommit1{})
+		// return ctx.Send(SectorRetrySealPreCommit1{})
 	}
 
 	return ctx.Send(SectorRetrySealPreCommit2{})
