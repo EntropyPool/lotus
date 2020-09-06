56258515d107d8359682fc4b37e9d6e66fa6dac2
liqh <codex8080@163.com>
2020-09-06 12:21:42 +0000

    Revert bad process of last err

--- extern/storage-sealing/fsm_events.go
+++ extern/storage-sealing/fsm_events.go
@@ -141,10 +141,7 @@ func (evt SectorSealPreCommit1Failed) FormatError(xerrors.Printer) (next error) 
 func (evt SectorSealPreCommit1Failed) apply(si *SectorInfo) {
 	si.InvalidProofs = 0 // reset counter
 	si.PreCommit2Fails = 0
-	if !strings.Contains(si.LastErr, "reserving storage space") &&
-		!strings.Contains(si.LastErr, "couldn't find a suitable path for a sector") {
-		si.PreCommit1Fails++
-	}
+	si.PreCommit1Fails++
 }
 
 type SectorSealPreCommit2Failed struct{ error }
