be980aac3c91904ae1a2667535ef3413288f9f28
Zhao KK <kikakkz@hotmail.com>
2021-02-02 18:01:08 +0800

    Do not use owner to commit

--- storage/addresses.go
+++ storage/addresses.go
@@ -57,7 +57,7 @@ func (as *AddressSelector) AddressFor(ctx context.Context, a addrSelectApi, mi m
 			addrs = append(addrs, a)
 		}
 	}
-	addrs = append(addrs, mi.Owner, mi.Worker)
+	addrs = append(addrs, /* mi.Owner, */mi.Worker)
 
 	return pickAddress(ctx, a, mi, goodFunds, minFunds, addrs)
 }
