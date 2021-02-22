36ef6fbe77039fb7e3f3f6f3ac28cafc4e835e8b
Zhao KK <kikakkz@hotmail.com>
2021-02-23 01:15:44 +0800

    Avoid concurrent of AP | PC2 | C2

--- extern/sector-storage/esched.go
+++ extern/sector-storage/esched.go
@@ -760,9 +760,10 @@ func (bucket *eWorkerBucket) tryPeekRequest() {
 	peekReqs := 0
 
 	for _, worker := range bucket.workers {
-		waitingJobs := bucket.waitingJobs(worker, sealtasks.TTPreCommit1)
-		waitingJobs += bucket.waitingJobs(worker, sealtasks.TTPreCommit2)
+		waitingJobs := bucket.waitingJobs(worker, sealtasks.TTPreCommit2)
 		waitingJobs += bucket.waitingJobs(worker, sealtasks.TTCommit2)
+		waitingJobs += worker.typedTaskCount(sealtasks.TTPreCommit2, false)
+		waitingJobs += worker.typedTaskCount(sealtasks.TTCommit2, false)
 
 		for _, pq := range worker.priorityTasksQueue {
 			for taskType, tq := range pq.typedTasksQueue {
